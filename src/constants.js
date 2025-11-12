a11ad1568e751d008f67645b021e15ddb387004e
Daniel Genov <danigenov@protonmail.com>
2025-11-13 00:36:09 +0100

    feat : add todo constructor

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const errors = {
+    title_empty_or_number : "Title can't be empty or be a number",
+    description_length : "Description must be between 10 and 500 characters",
+    dateFormat : "Date format must be day/month/year",
+    priority : "Only priority options are low, medium and high"
+}
+
+export {errors};
---
+++ src/todoitem.js
@@ -0,0 +1,14 @@
+import { errors } from "./constants.js";
+
+class ToDoItem {
+    constructor(title, description, dueDate, priority) {
+
+        if (typeof(title) !== "string" || title.length == 0) throw new Error(errors["title_empty_or_number"]);
+        if (description.length == 0 || description.length < 10 || description.length > 500) throw new Error(errors["description_length"]);
+        if (priority != "high" && priority != "low" && priority != "medium") throw new Error(errors["priority"]);
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
