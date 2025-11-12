2268f745d812bc9cd7cde070aa8d94a69e982141
Daniel Genov <danigenov@protonmail.com>
2025-11-13 00:42:48 +0100

    feat : add todo creation objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+import {createToDo} from "./todoitems.js";
+
+let firstToDo = createToDo("Finish this project", "Finish the ToDoApp for my github portfolio", "18-11-2025", "medium");
+
+console.log("FIRST TODO: \n");
+for (const key in firstToDo) {
+    console.log(`${key} : ${firstToDo[key]}\n`);
+}
--- src/todoitems.js
+++ src/todoitems.js
@@ -6,9 +6,22 @@ class ToDoItem {
         if (typeof(title) !== "string" || title.length == 0) throw new Error(errors["title_empty_or_number"]);
         if (description.length == 0 || description.length < 10 || description.length > 500) throw new Error(errors["description_length"]);
         if (priority != "high" && priority != "low" && priority != "medium") throw new Error(errors["priority"]);
+
+        
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
     }
 }
+
+function createToDo(title, description, dueDate, priority) {
+    let to_do_object = new ToDoItem(title, description, dueDate, priority);
+    return to_do_object;
+}
+
+function addToDoDOM() {
+    // todo
+}
+
+export {createToDo};
